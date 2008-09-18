f6091bfeb34d508949e94251db0e812601603434
Robert Gentleman <rgentlem@fhcrc.org>
2008-09-18 18:29:17 +0000

    working on new funs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@33805 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/funs.R
+++ R/funs.R
@@ -20,14 +20,41 @@
     byCIR
 }
 
-combineRanges <- function(rlist)
-{
-    IRanges(start = unlist(lapply(rlist, start), use.names = FALSE),
-            end = unlist(lapply(rlist, end), , use.names = FALSE))
+##take two or more lanes and combine the data
+combineLanes <- function(laneList, chromList = names(laneList[[1]])) {
+    names(chromList) = chromList ##to get the return value named
+    lapply(chromList, function(x) combineRanges(laneList, x))
 }
 
+##one might also want a one argument version, but for now, this seems
+##to be somewhat effective
+combineRanges <- function(rlist, byChr)
+{
+    IRanges(start = unlist(lapply(rlist, function(x)
+            start(x[[byChr]])), use.names = FALSE),
+            end = unlist(lapply(rlist, function(x)  end(x[[byChr]])),
+            use.names = FALSE))
+}
 
-##FIXME: a lot of this functionality will become available in 
+##subsampling two "lanes", so that on a per chromosome basis they have
+##the same number of reads; let's not do this if we are reasonably
+##close - when fudge
+laneSubsample <- function(lane1, lane2, fudge = 0.05)
+{
+    chromList = names(lane1) ##lane2 should have the same names
+    l1Len = sapply(lane1, length)
+    l2Len = sapply(lane2, length)
+    for(i in 1:length(l1Len) ) {
+        if(abs(l1Len[i]-l2Len[i])/l1Len[i] < fudge) next
+        if(l1Len[i] < l2Len[i])
+            lane2[[i]] = sample(lane2[[i]], l1Len[i])
+        if(l1Len[i] > l2Len[i])
+            lane1[[i]] = sample(lane2[[i]], l2Len[i])
+    }
+    return(list(lane1=lane1, lane2=lane2))
+}
+
+##Fixme: a lot of this functionality will become available in 
 ##ReadAligned from the ShortRead package
 readAndClean <- function(maqDir, pattern = pat, exclude="random", 
    dropDups = TRUE, minScore = 15 ) 
