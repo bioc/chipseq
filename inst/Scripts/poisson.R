19bf7dc56bb2686a6da1234a0dbee87a3626de40
Deepayan Sarkar <dsarkar@fhcrc.org>
2008-09-26 20:13:34 +0000

    updates
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@33995 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Scripts/poisson.R
+++ inst/Scripts/poisson.R
@@ -107,7 +107,7 @@ ref.peaks.in.ctrl <-
     copyIRangesbyChr(ref.peaks, covctrl)
 
 
-
+## Switch roles (ref vs response).  Beware of wrong column names
 if (FALSE)
 {
     ref.peaks <- lapply(covblasts, slice, lower = 10)
@@ -122,7 +122,9 @@ peakSummary <-
     do.call(make.groups,
             sapply(names(ref.peaks),
                    function(chr) {
-                       data.frame(reads.tubes = viewSums(ref.peaks[[chr]]) / 200,
+                       data.frame(start = start(ref.peaks[[chr]]),
+                                  end = end(ref.peaks[[chr]]),
+                                  reads.tubes = viewSums(ref.peaks[[chr]]) / 200,
                                   reads.blasts = viewSums(ref.peaks.in.blasts[[chr]]) / 200,
                                   reads.ctrl = viewSums(ref.peaks.in.ctrl[[chr]]) / 200)
                    },
@@ -132,25 +134,25 @@ names(peakSummary)[names(peakSummary) == "which"] <- "chromosome"
 
 xyplot(sqrt(reads.blasts) + sqrt(3 * reads.ctrl) ~ sqrt(reads.tubes) | chromosome,
        data = peakSummary, auto.key = TRUE,
-       subset = chromosome %in% c("chr1", "chr2"),
+       subset = chromosome %in% c("chr1", "chr2", "chr3", "chr4"),
        par.settings = simpleTheme(pch = 16, alpha = 0.4),
-       type = c("p", "g", "r"), aspect = "iso")
+       type = c("p", "g", "r"), col.line = "black", aspect = "iso")
 
 
 xyplot(sqrt(reads.blasts) ~ sqrt(reads.tubes) | chromosome,
        data = peakSummary, auto.key = TRUE,
-       subset = (chromosome %in% c("chr1", "chr2") &
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4") &
                  reads.ctrl < quantile(reads.ctrl, 0.99)),
        par.settings = simpleTheme(pch = ".", cex = 3),
-       type = c("p", "g", "r"), aspect = "iso")
+       type = c("p", "g", "r"), col.line = "black", aspect = "iso")
 
 
 xyplot(log2(1+reads.blasts) ~ log2(1+reads.tubes) | chromosome,
        data = peakSummary, auto.key = TRUE,
-       subset = (chromosome %in% c("chr1", "chr2") &
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4") &
                  reads.ctrl < quantile(reads.ctrl, 0.99)),
        par.settings = simpleTheme(pch = ".", cex = 3),
-       type = c("p", "g", "r"), aspect = "iso")
+       type = c("p", "g", "r"), col.line = "black", aspect = "iso")
 
 
 ## Naive regression: y|x ~ Poisson(cx), i.e., mean=cx, variance=cx.
@@ -158,10 +160,9 @@ xyplot(log2(1+reads.blasts) ~ log2(1+reads.tubes) | chromosome,
 ## weights=1/sqrt(variance)=1/sqrt(x).  This ignores that Poisson has
 ## no scale factor to estimate, but we probably have some variance
 ## inflation anyway.  The other option, of course, is to use glm().
+## UPDATE: the proper weights seem to be 1/x.
 
-library(robustbase)
-
-peakSummary <- 
+peakSummary.lm <- 
     within(peakSummary,
        {
            fm <- lm(sqrt(reads.blasts) ~ 0 + chromosome:sqrt(reads.tubes),
@@ -176,25 +177,88 @@ peakSummary <-
 
 
 xyplot(resid.blasts ~ log2(reads.tubes) | chromosome,
-       data = peakSummary, auto.key = TRUE,
-       subset = (chromosome %in% c("chr1", "chr2") &
-                 reads.ctrl < quantile(reads.ctrl, 0.99)),
+       data = peakSummary.lm, auto.key = TRUE,
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
        par.settings = simpleTheme(pch = ".", cex = 3),
        type = c("p", "g", "smooth"), col.line = "black")
-
 
 xyplot(abs(resid.blasts) ~ log2(reads.tubes) | chromosome,
-       data = peakSummary, auto.key = TRUE,
-       subset = (chromosome %in% c("chr1", "chr2") &
-                 reads.ctrl < quantile(reads.ctrl, 0.99)),
+       data = peakSummary.lm, auto.key = TRUE,
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
+       par.settings = simpleTheme(pch = ".", cex = 3),
+       type = c("p", "g", "smooth"), col.line = "black")
+
+qqmath(~resid.blasts | chromosome,
+       data = peakSummary.lm, 
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
+       par.settings = simpleTheme(pch = ".", cex = 3),
+       panel = function(...) {
+           panel.abline(0, 1)
+           panel.qqmath(...)
+       },
+       type = c("p", "g"), col.line = "black", aspect = "iso")
+
+bwplot(chromosome ~ resid.blasts, data = peakSummary.lm)
+
+
+## library(robustbase)
+
+## A desirable alternative is robust regression, but lmrob in
+## robustbase doesn't seem to handle weights.  The log2 responses seem
+## reasoanbly homoskedastic.  
+
+
+xyplot(abs(log2(reads.blasts)-log2(reads.tubes)) ~ log2(reads.tubes) | chromosome,
+       data = peakSummary.lm, auto.key = TRUE,
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
+       par.settings = simpleTheme(pch = ".", cex = 3),
+       type = c("p", "g", "smooth"), col.line = "black")
+
+## Our model of y ~ c.x translates to log(y) ~ log(c) + log(x), so all
+## we need to do is estimate log(c). A robust estimate is
+## median(log(y)-log(x)).  So residuals are
+##  [ (log(y)-log(x)) - median(log(y)-log(x)) ]
+
+
+peakSummary.rob <- 
+    within(peakSummary,
+       {
+           diffs <- log2(reads.blasts)-log2(reads.tubes)
+           resids <- diffs - median(diffs)
+       })
+
+median(peakSummary.rob$diffs)
+
+2^median(peakSummary.rob$diffs)
+sum(sapply(cblasts, length)) / sum(sapply(ctubes, length))
+
+xyplot(resids ~ log2(reads.tubes) | chromosome,
+       data = peakSummary.rob, auto.key = TRUE,
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
        par.settings = simpleTheme(pch = ".", cex = 3),
        type = c("p", "g", "smooth"), col.line = "black")
 
 
+xyplot(abs(resids) ~ log2(reads.tubes) | chromosome,
+       data = peakSummary.rob, auto.key = TRUE,
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
+       par.settings = simpleTheme(pch = ".", cex = 3),
+       type = c("p", "g", "smooth"), col.line = "black")
 
+qqmath(~resids | chromosome,
+       data = peakSummary.rob, 
+       subset = (chromosome %in% c("chr1", "chr2", "chr3", "chr4")),
+       par.settings = simpleTheme(pch = ".", cex = 3),
+       panel = function(...) {
+           panel.abline(0, 1)
+           panel.qqmath(...)
+       },
+       type = c("p", "g"), col.line = "black", aspect = "iso")
 
+bwplot(chromosome ~ resids, data = peakSummary.rob)
 
-
-
+toppeaks <- subset(peakSummary.rob, abs(resids) > 4)
+rownames(toppeaks) <- NULL
+toppeaks[rev(order(abs(toppeaks$resids))), 1:6]
 
 
