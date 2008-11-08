39f734abc384720b99c05a2140096bd6d54607b9
Robert Gentleman <rgentlem@fhcrc.org>
2008-11-08 20:08:53 +0000

    tidying up a few loose ends
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@35164 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/islands.R
+++ R/islands.R
@@ -1,3 +1,25 @@
+
+logscale.components <- function(axis = c("x", "y"), base = 2)
+{
+    axis <- match.arg(axis)
+    switch(axis,
+           x = 
+           function(...) {
+               ans <- xscale.components.default(...)
+               ans$bottom$labels$labels <- 
+                   base^(ans$bottom$labels$at)
+               ans
+           },
+           y = 
+           function(...) {
+               ans <- yscale.components.default(...)
+               ans$left$labels$labels <- 
+                   base^(ans$left$labels$at)
+               ans
+           })
+}
+
+
 ## common infrastructure to summarize reads in the form of nested lists: 
 ##  reads.list=list("1" = list("chr1" = list("+"=..., "-"=...), 
 ##                             "chr2"=...), 
@@ -14,7 +36,7 @@ summarizeLane <- function(clist, summary.fun, ...)
 
 
 summarizeReads <- 
-    function(reads.list, lanes = c("1", "2", "3", "4", "6", "7", "8"), ..., verbose = FALSE)
+    function(reads.list, lanes = names(reads.list), ..., verbose = FALSE)
 {
     if (verbose) cat(paste("Processing lanes", paste(lanes, collapse = ",")), fill = TRUE)
     ans <- do.call(lattice::make.groups, lapply(reads.list[lanes], summarizeLane, ...))
