6d5c19c91f42625359ae9217ac61c8b588135583
Robert Gentleman <rgentlem@fhcrc.org>
2008-11-08 19:39:33 +0000

    added the island code from Scripts/islands.Rnw to the package so we don't need to run the Rnw to get the functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@35162 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/islands.R
@@ -0,0 +1,62 @@
+## common infrastructure to summarize reads in the form of nested lists: 
+##  reads.list=list("1" = list("chr1" = list("+"=..., "-"=...), 
+##                             "chr2"=...), 
+##                  "2" = list(...))
+
+summarizeLane <- function(clist, summary.fun, ...)
+{
+    ## clist is a list at the lane level, with one list("+"=, "-"=) for each chromsome
+    ans <- do.call(lattice::make.groups, lapply(clist, summary.fun, ...))
+    names(ans)[names(ans) == "which"] <- "chromosome"
+    ## cbind(chr = factor(colnames(ans), levels = colnames(ans)), as.data.frame(t(ans)))
+    ans
+}
+
+
+summarizeReads <- 
+    function(reads.list, lanes = c("1", "2", "3", "4", "6", "7", "8"), ..., verbose = FALSE)
+{
+    if (verbose) cat(paste("Processing lanes", paste(lanes, collapse = ",")), fill = TRUE)
+    ans <- do.call(lattice::make.groups, lapply(reads.list[lanes], summarizeLane, ...))
+    names(ans)[names(ans) == "which"] <- "lane"
+    ans
+}
+
+## different summary.fun can give different useful summaries    
+
+countSummary <- function(x) 
+{
+    ## x is a list at the lane->chromosome level, with components "+" and "-"
+    npos <- length(x$"+")
+    nneg <- length(x$"-")
+    data.frame(n = npos + nneg, d = npos - nneg, r = npos / nneg)
+}    
+
+## get islands.  But this needs more care; 
+## coverage by chromosome, possibly different species.  
+## We'll avoid this by just using the furthest hit
+
+## library("BSgenome.Mmusculus.UCSC.mm9")
+## library("BSgenome.Hsapiens.UCSC.hg18")
+
+## mouse.seqlens <- seqlengths(Mmusculus)
+## human.seqlens <- seqlengths(Hsapiens)
+
+sliceSummary <- 
+    function(x, lower = 1,
+             viewSummary = list(sums = viewSums, maxs = viewMaxs))
+    ## x is a list at the lane->chromosome level, with components "+" and "-"
+{
+    g <- growSeqs(x)
+    cov <- coverage(g, 1, max(end(g) + 400L))
+    s <- slice(cov, lower = lower)
+    ans <- data.frame(start = start(s), end = end(s))
+    if (is.list(viewSummary)) 
+    {
+        for (nm in names(viewSummary))
+            ans[[nm]] <- viewSummary[[nm]](s)
+    }
+    else ans[["summary"]] <- viewSummary(s)
+    ans
+}
+
