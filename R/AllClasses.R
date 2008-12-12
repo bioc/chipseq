4c312b21bd8dff1a1db7c2446b4f2f253d17990d
Deepayan Sarkar <dsarkar@fhcrc.org>
2008-12-12 00:33:00 +0000

    correct version of data
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@35867 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -5,3 +5,20 @@ setClass("GenomeList",
          contains = "list",
          representation(genome = "character"))
 
+
+## Long-term goal: use BSgenome classes "ChromosomeData", "SampleChromosomeData"
+
+setAs("list", "ChromosomeData",
+      function(from) {
+          new("ChromosomeData",
+              elements = from)
+      })
+
+
+setAs("list", "SampleChromosomeData",
+      function(from) {
+          el <- lapply(from, function(x) as(x, "ChromosomeData"))
+          new("SampleChromosomeData", elements = el)
+      })
+
+
