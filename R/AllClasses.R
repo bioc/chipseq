1194fa780c7e9edac2391e19bdafddcd3935aa0d
Deepayan Sarkar <dsarkar@fhcrc.org>
2009-01-14 23:29:56 +0000

    new gdApply function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@36657 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -6,19 +6,93 @@ setClass("GenomeList",
          representation(genome = "character"))
 
 
-## Long-term goal: use BSgenome classes "ChromosomeData", "SampleChromosomeData"
+## Long-term goal: use BSgenome classes "GenomeData", "GenomeDataList"
 
-setAs("list", "ChromosomeData",
-      function(from) {
-          new("ChromosomeData",
-              elements = from)
-      })
+## setAs("list", "ChromosomeData",
+##       function(from) {
+##           new("ChromosomeData",
+##               elements = from)
+##       })
 
 
-setAs("list", "SampleChromosomeData",
-      function(from) {
-          el <- lapply(from, function(x) as(x, "ChromosomeData"))
-          new("SampleChromosomeData", elements = el)
-      })
+## setAs("list", "SampleChromosomeData",
+##       function(from) {
+##           el <- lapply(from, function(x) as(x, "ChromosomeData"))
+##           new("SampleChromosomeData", elements = el)
+##       })
 
+## 
+
+
+setGeneric("gdApply",
+           function(X, FUN, ...) {
+               standardGeneric("gdApply")
+           })
+
+setMethod("gdApply",
+          signature(X = "GenomeDataList", FUN = "function"),
+          function(X, FUN, ...) {
+              NX <- length(X)
+              new.elements <- vector(mode = "list", length = NX)
+              names(new.elements) <- names(X)
+              for (i in seq_len(NX))
+              {
+                  new.elements[[i]] <- gdApply(X[[i]], FUN, ...)
+              }
+              cls <- lapply(new.elements, class)
+              ucl <- unique(unlist(cls))
+              if (identical(ucl, "GenomeData"))
+                  GenomeDataList(new.elements)
+              else new.elements
+          })
+
+setMethod("gdApply",
+          signature(X = "GenomeData", FUN = "function"),
+          function(X, FUN, ...) {
+              NX <- length(X)
+              new.elements <- vector(mode = "list", length = NX)
+              names(new.elements) <- names(X)
+              for (i in seq_len(NX))
+              {
+                  new.elements[[i]] <- FUN(X[[i]], ...)
+              }
+              cls <- lapply(new.elements, class)
+              ucl <- unique(unlist(cls))
+              if (length(ucl) == 1)
+                  GenomeData(new.elements, organism = organism(X))
+              else new.elements
+          })
+
+
+
+
+
+
+## summarizeLane <- function(clist, summary.fun, ...)
+## {
+##     ## clist is a list at the lane level, with one list("+"=, "-"=) for each chromsome
+##     ans <- do.call(lattice::make.groups, lapply(clist, summary.fun, ...))
+##     names(ans)[names(ans) == "which"] <- "chromosome"
+##     ## cbind(chr = factor(colnames(ans), levels = colnames(ans)), as.data.frame(t(ans)))
+##     ans
+## }
+
+## summarizeLane2 <- function(clist, summary.fun, ..., seqlen)
+## {
+##     ## clist is a list at the lane level, with one list("+"=, "-"=) for each chromsome
+##     stopifnot(all(names(clist) %in% names(seqlen)))
+##     seqlen <- seqlen[names(clist)]
+##     mapply(summary.fun, clist, seqlen, ..., SIMPLIFY = FALSE)
+## }
+
+
+
+## summarizeReads <- 
+##     function(reads.list, lanes = names(reads.list), ..., verbose = FALSE)
+## {
+##     if (verbose) cat(paste("Processing lanes", paste(lanes, collapse = ",")), fill = TRUE)
+##     ans <- do.call(lattice::make.groups, lapply(reads.list[lanes], summarizeLane, ...))
+##     names(ans)[names(ans) == "which"] <- "lane"
+##     ans
+## }
 
