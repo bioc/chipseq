3cd6a9cf3a56717d12569580e146a4d9714a5c4d
Patrick Aboyoun <unknown>
2009-03-11 22:56:41 +0000

    First pass at code speed up.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@37910 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/stats.R
@@ -0,0 +1,68 @@
+basesCovered <- function(x, shift = seq(0, 500, 5), seqLen = 35)
+{
+    maxShift <- max(shift)
+    rng <- range(unlist(x)) + c(-1, 1) * maxShift
+    cov.pos <- coverage(extendReads(x, seqLen = seqLen, strand = "+"), rng[1], rng[2]) > 0
+    cov.neg <- coverage(extendReads(x, seqLen = seqLen, strand = "-"), rng[1], rng[2]) > 0
+    n <- diff(rng) + 1L
+    ans <-
+      sapply(shift,
+             function(s) {
+                 cat("\r", s, "/", maxShift)
+                 sum(subseq(cov.pos, start = 1L, end = n - s) |
+                     subseq(cov.neg, start = s + 1L, end = n))
+             })
+    cat("\n")
+    data.frame(mu = seqLen + shift, covered = ans / ans[1])
+}
+
+
+densityCorr <- function(x, shift = seq(0, 500, 5), ...)
+{
+    nShift <- length(shift)
+    maxShift <- max(shift)
+    rng <- range(unlist(x)) + c(-1, 1) * maxShift
+    d1 <- density(x$"+", from = rng[1], to = rng[2],
+                  n = round(diff(rng) / 2), ...)
+    d2 <- density(x$"-", from = rng[1], to = rng[2] + maxShift,
+                  n = round((diff(rng) + maxShift) / 2), ...)
+    d2Shifted <- approx(d2, xout = as.vector(outer(d1$x, shift, FUN = "+")))
+    d2Y <- matrix(d2Shifted$y, ncol = nShift)
+    data.frame(mu = shift, corr = as.vector(cor(d1$y, d2Y)))
+}
+
+
+
+## Jothi et al method
+## For every tag i in the sense strand, the nearest tag k
+## in the antisense strand, downstream of i, is identified. Let j be the
+## tag in the sense strand immediately upstream of k. Note that i and j
+## could be the same tag. The mean DNA fragment length F is given by
+
+## 2/n \sum_i (d(i,j) + d(j,k)/2),
+
+## where n is the number of sense tags for which there exists a
+## k and a j tag, and d(i,k)<=500
+
+jothi.estimate <- function(x, maxDist = 500L)
+{
+    i <- x$"+"
+    message(length(i), " reads")
+    pos <- sort(unique(x$"+"))
+    neg <- sort(unique(x$"-"))
+
+    whichK <- findInterval(i, neg) + 1L
+    whichK[whichK > length(neg)] <- NA_integer_
+    k <- neg[whichK]
+
+    whichJ <- findInterval(k - 1L, pos)
+    whichJ[whichJ < 1L] <- NA_integer_
+    j <- pos[whichJ]
+
+    keep <- !is.na(j) & !is.na(k) & abs(i-k) < maxDist
+    i <- i[keep]
+    j <- j[keep]
+    k <- k[keep]
+
+    2 * sum(abs(i-j) + 0.5 * abs(j-k)) / sum(keep)
+}
