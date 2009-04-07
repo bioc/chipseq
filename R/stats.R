ab1df7bb92374baf04e6a484a9187c4e01c7a318
Deepayan Sarkar <dsarkar@fhcrc.org>
2009-04-07 16:10:42 +0000

    moved sparse density calculation code into chipseq
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@38558 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/stats.R
+++ R/stats.R
@@ -1,3 +1,136 @@
+
+
+## The first set of functions provide a version of density for
+## 'sparse' data (large stretches of empty space where density is 0;
+## so large that standard density() would not be able to compute it.
+## FIXME: this is definitely not the right place to put this, but I'm
+## not sure what is (IRanges has the underlying classes, but that does
+## not necessarily make it the right choice).
+
+## The use-case of interest is Kharchencko et al's density-correlation
+## method for estimating average fragmet length
+
+
+dKernel <-
+    function(width = 50,
+             kernel = c("gaussian", "epanechnikov", "rectangular", "triangular", "biweight", "cosine", "optcosine"))
+{
+    kernel <- match.arg(kernel) 
+    kords <- seq(-width, width)
+    switch(kernel,
+           gaussian = dnorm(kords, sd = width / 3),
+           rectangular = {
+               a <- width
+               ifelse(abs(kords) < a, 0.5/a, 0)
+           }, triangular = {
+               a <- width
+               ax <- abs(kords)
+               ifelse(ax < a, (1 - ax/a)/a, 0)
+           }, epanechnikov = {
+               a <- width
+               ax <- abs(kords)
+               ifelse(ax < a, 3/4 * (1 - (ax/a)^2)/a, 0)
+           }, biweight = {
+               a <- width
+               ax <- abs(kords)
+               ifelse(ax < a, 15/16 * (1 - (ax/a)^2)^2/a, 0)
+           }, cosine = {
+               a <- width
+               ifelse(abs(kords) < a, (1 + cos(pi * kords/a))/(2 * a), 0)
+           }, optcosine = {
+               a <- width
+               ifelse(abs(kords) < a, pi/4 * cos(pi * kords/(2 * a))/a, 0)
+           })
+}
+
+
+
+## pad a 0 in the beginning to fill in stretch of 0's before
+
+doDensity1 <- function(x, kernel, width) ## calls R's density() 
+{
+    n <- length(x)
+    from <- x[1] - width
+    to <- x[n] + width
+    c(0, n * density(x, kernel = kernel, width = 2 * width, from = from, to = to, n = to - from + 1)$y)
+}
+
+
+
+doDensity2 <- function(x, dk, width) ## explicit looping (but dk not recomputed)
+{
+    n <- length(x)
+    if (n == 1)
+    {
+        c(0, dk)
+    }
+    else
+    {
+        x <- x - (x[1] - width - 1)
+        from <- x[1] - width
+        to <- x[n] + width
+        len <- to - from + 1
+        ans <- numeric(len)
+        for (xi in x)
+        {
+            si <- seq(xi - width, xi + width)
+            ans[si] <- ans[si] + dk
+        }
+        c(0, ans)
+    }
+}
+
+## experimental=FALSE calls R's density() within each "island".
+## experimental=TRUE pre-computes the kernel function for the given
+## width and contructs the density by looping in R.  This gives a ~60x
+## speedup (most islands have few reads, and the loops are short
+## there).  This (the tapply part) can easily be ported to C to be
+## even faster.
+
+sparse.density <- function(x, width = 50, kernel = "epanechnikov", experimental = TRUE,
+                           from = start(rix)[1] - 10L,
+                           to = end(rix)[length(rix)] + 10L)
+{
+    x <- sort(x)
+    ix <- IRanges(x-width, x+width)
+    rix <- sort(reduce(ix))
+    ## we will calculate density on a range containing the data.  If
+    ## necessary, we will subset later (FIXME: TODO).
+    from0 <- min(from, start(rix)[1] - 10L)
+    to0 <- max(to, end(rix)[length(rix)] + 10L)
+    if (from > from0 || to < to0) stop("[from, to] smaller than support not implemented yet (but easy to add)")
+    ## ox <- overlap(rix, x, multiple = FALSE)
+    ox <- findInterval(x, start(rix)) # equivalent, but a little faster
+    island.densities <- 
+        if (!experimental)
+        {
+            tapply(x, ox, doDensity, kernel = kernel, width = width, simplify = FALSE)
+        }
+        else 
+        {
+            dk <- dKernel(width = width, kernel = kernel)
+            tapply(x, ox, doDensity2, dk = dk, width = width, simplify = FALSE)
+        }
+    ## result will be a "Rle" object.  Need to compute @values and
+    ## @lengths.  We have the pieces, each of which will have
+    ## values=island.densities[[i]][-1] and lengths=rep(1,width(rix)).
+    ## In between, we have stretches of 0.
+    zero.lengths <- c(start(rix), to0) - c(from0, end(rix)) - 1L
+    all.values <- c(unlist(island.densities, recursive = FALSE, use.names = FALSE), 0)
+    all.lengths <- rep(1L, length(all.values))
+    all.lengths[cumsum(c(1L, sapply(island.densities, length)))] <- zero.lengths
+    ## ans <- Rle(all.values, all.lengths) # has unnecessary fancy checks
+    ans <- new("Rle", values = all.values, lengths = as.integer(all.lengths))
+    ans
+}
+
+
+
+
+
+
+
+
 basesCovered <- function(x, shift = seq(0, 500, 5), seqLen = 35, verbose = FALSE)
 {
     maxShift <- max(shift)
