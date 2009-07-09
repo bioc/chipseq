dc1f039852832081a5dbf0ad95367431da36f0d8
Patrick Aboyoun <unknown>
2009-07-09 23:52:56 +0000

    Removed unused variables in C code.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@40585 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/rlesumprod.c
+++ src/rlesumprod.c
@@ -3,19 +3,17 @@
 #include <Rinternals.h>
 #include <Rmath.h>
 
-SEXP 
-rle_sum_prod(SEXP px1, SEXP pn1, SEXP ps1, 
-	     SEXP px2, SEXP pn2, SEXP ps2, 
+SEXP
+rle_sum_prod(SEXP px1, SEXP pn1, SEXP ps1,
+	     SEXP px2, SEXP pn2, SEXP ps2,
 	     SEXP plen)
 {
-    double 
+    double
 	ans,
 	*x1 = REAL(px1),
 	*x2 = REAL(px2);
-    int 
+    int
 	i1, i2, k, next_k,
-	*n1 = INTEGER(pn1),
-	*n2 = INTEGER(pn2),
 	*s1 = INTEGER(ps1),
 	*s2 = INTEGER(ps2),
 	len = asInteger(plen);
@@ -43,17 +41,15 @@ rle_sum_prod(SEXP px1, SEXP pn1, SEXP ps1,
 
 
 
-SEXP 
-rle_sum_any(SEXP px1, SEXP pn1, SEXP ps1, 
-	    SEXP px2, SEXP pn2, SEXP ps2, 
+SEXP
+rle_sum_any(SEXP px1, SEXP pn1, SEXP ps1,
+	    SEXP px2, SEXP pn2, SEXP ps2,
 	    SEXP plen)
 {
-    int 
+    int
 	i1, i2, k, next_k, ans,
 	*x1 = INTEGER(px1),
 	*x2 = INTEGER(px2),
-	*n1 = INTEGER(pn1),
-	*n2 = INTEGER(pn2),
 	*s1 = INTEGER(ps1),
 	*s2 = INTEGER(ps2),
 	len = asInteger(plen);
@@ -71,22 +67,22 @@ rle_sum_any(SEXP px1, SEXP pn1, SEXP ps1,
 
 
 
-SEXP 
+SEXP
 do_naive_density(SEXP x, SEXP dk, SEXP width)
 {
     SEXP ans;
     double *pans;
-    int 
+    int
 	i, j, xi,
-	n = length(x), 
+	n = length(x),
 	m = length(dk),
-	x1 = INTEGER(x)[0], 
+	x1 = INTEGER(x)[0],
 	xn = INTEGER(x)[n-1];
     ans = PROTECT(allocVector(REALSXP, xn - x1 + 2 + 2 * asInteger(width)));
     pans = REAL(ans);
     for (i = 0; i < length(ans); i++) pans[i] = 0;
     /* for each point xi in x, add dk suitably shifted */
-    for (i = 0; i < n; i++) { 	
+    for (i = 0; i < n; i++) {
 	xi = INTEGER(x)[i] - x1 + 1;
 	for (j = 0; j < m; j++) {
 	    pans[xi + j] += REAL(dk)[j];
