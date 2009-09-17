baf58830d3103f30c315089a42289fb68c09170f
Herve Pages <hpages@fhcrc.org>
2009-09-17 20:32:13 +0000

    #oops, one more
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chipseq@41732 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Scripts/diffpeaks.R
+++ inst/Scripts/diffpeaks.R
@@ -24,8 +24,9 @@ computeOverlap <- function(chr, start, end, tchr, tstart, tend)
     {
         id <- which(chr == chrom)
         ans[id] <-
-            !is.na(overlap(tsplit[[chrom]], 
+            !is.na(findOverlaps(
                            IRanges(start[id], end[id]),
+                           tsplit[[chrom]],
                            multiple = FALSE))
     }
     ans
